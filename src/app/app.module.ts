20032efd57909844ed9ffbdb1157b4fa8b8c8350
Beshoy Refky Mansour <beshoy.refky.mansour@gmail.com>
2017-09-26 14:34:25 +0200

    Fist time

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MoviesStartComponent } from './movies/movies-start/movies-start.component';
+import { MoviesListComponent } from './movies/movies-list/movies-list.component';
+import { MovieItemComponent } from './movies/movie-item/movie-item.component';
+import { MovieEditComponent } from './movies/movie-edit/movie-edit.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SignupComponent,
+    SigninComponent,
+    MoviesComponent,
+    MoviesStartComponent,
+    MoviesListComponent,
+    MovieItemComponent,
+    MovieEditComponent,
+    ShoppingListComponent
   ],
   imports: [
     BrowserModule
